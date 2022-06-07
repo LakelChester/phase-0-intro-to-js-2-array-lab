c1bf3107519fa319dedd9cf4d972b44b144a2339
LakelChester <lakel@chesterassociate.com>
2022-06-07 15:57:57 -0700

    commit tree 3cdd03568c8c88d32fa566f6f4d5209738e69acd

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendCat(name) {
+    console.log(cats.push("Ralph"), name);
+  }
+  
+  function destructivelyPrependCat(name){
+    console.log(cats.unshift("Bob"),name)
+  }
+
+  function destructivelyRemoveLastCat(){
+    console.log(cats.pop())
+  }
+
+  function destructivelyRemoveFirstCat(){
+    console.log(cats.shift())
+  }
+
+  function appendCat(name){
+    const newCats = [...cats, name]
+    return newCats
+  }
+   
+  function prependCat(name){
+    const copyNewCats = [name, ...cats]
+    return copyNewCats
+  }
+
+  const removeLastCat = (name) => {
+    let newRemoveLastCat = [...cats];
+    newRemoveLastCat.pop(name);
+    return newRemoveLastCat;
+}
+
+const removeFirstCat = (name) => {
+  let newRemoveFirstCat = [...cats];
+  newRemoveFirstCat.shift(name);
+  return newRemoveFirstCat;
+}
